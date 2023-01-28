65a49cb75ed0da16216aaf1fef7ee21b59b62083
DezBooM <dezboom3@gmail.com>
2023-01-28 15:25:03 +0100

    changed layout for navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,16 +5,16 @@ import Search from "./Search"
 function Navbar({ dark, handleTheme }) {
   return (
     <div>
-      <div className="flex top-0 items-center justify-between px-1 sm:px-5 bg-emerald-500 h-12 lg:h-16 dark:bg-neutral-800">
+      <div className="flex items-center justify-between px-1 sm:px-5 lg:px-10 bg-emerald-500 h-12 lg:h-16 dark:bg-neutral-800">
         <Link to="/" className="outline-none">
-          <h1 className="font-aboreto text-sm sm:text-3xl tracking-tighter w-24 h-10 sm:w-full sm:h-full px-2 sm:px3 sm:py-1 rounded-lg bg-emerald-900 dark:bg-neutral-900">
+          <h1 className="font-aboreto text-sm sm:text-3xl tracking-tighter w-24 h-10 sm:w-full sm:h-full px-2 sm:px3 sm:py-1 lg:mt-9 rounded-lg bg-emerald-900 dark:bg-neutral-900">
             CINEMA TREASURE
           </h1>
         </Link>
         <Search />
         <button
           type="button"
-          className="p-2 rounded-full outline-none bg-emerald-900 dark:bg-neutral-900 text-center"
+          className="p-2 lg:mt-9 rounded-full outline-none bg-emerald-900 dark:bg-neutral-900 text-center"
           onClick={handleTheme}
         >
           {!dark ? "☀️" : "🌑"}
