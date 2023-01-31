eb393e834fa53085289e28cea956764635e67e8c
DezBooM <dezboom3@gmail.com>
2023-01-31 12:04:15 +0100

    fixed bugs

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -5,19 +5,19 @@ function Category() {
   return (
     <div className="flex border-b-4 border-emerald-100 dark:border-yellow-500">
       <NavLink
-        to="/#/popular"
+        to="/popular"
         className="current:bg-emerald-900 current:dark:bg-neutral-900 px-2 py-1 rounded-t-lg outline-none"
       >
         <h1 className="">POPULAR</h1>
       </NavLink>
       <NavLink
-        to="/#/top-rated"
+        to="/top-rated"
         className="current:bg-emerald-900 current:dark:bg-neutral-900 px-2 py-1 rounded-t-lg outline-none"
       >
         <h1 className="">TOP-RATED</h1>
       </NavLink>
       <NavLink
-        to="/#/upcoming"
+        to="/upcoming"
         className="current:bg-emerald-900 current:dark:bg-neutral-900 px-2 py-1 rounded-t-lg outline-none"
       >
         <h1 className="">UPCOMING</h1>
